9f0d6fe20371d8a9701b0220eca94eacb352e985
dave <davidmasq@gmail.com>
2024-12-06 18:07:03 -0800

    wouldnt be AoC without using some concurrency poorly

--- 06/main.go
+++ 06/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"slices"
 	"strings"
+	"sync"
 )
 
 type direction int
@@ -17,27 +18,23 @@ const (
 	LEFT
 )
 
-type bump struct {
-	x, y int
-}
-
 func part1(inp string) int {
 	count := 0
 	split := strings.Split(inp, "\n")
 	split = split[:len(split)-1]
-	posx, posy := 0, 0
 	dir := UP
 	seen := make(map[[2]int]bool)
-	for i := range split {
-		for j := range split[i] {
-			if split[i][j] == '^' {
-				posx = j
-				posy = i
-				goto cont
+	posx, posy := func() (int, int) {
+		for i := range split {
+			for j := range split[i] {
+				if split[i][j] == '^' {
+					return j, i
+				}
 			}
 		}
-	}
-cont:
+		return 0, 0
+	}()
+
 	for {
 		count++
 		seen[[2]int{posy, posx}] = true
@@ -321,33 +318,88 @@ func checkForLoopArr(posx, posy int, d direction, split [][]rune) bool {
 	}
 }
 
+func checkForLoopArrPar(locx, locy int, posx, posy int, d direction, split [][]rune) bool {
+	startx := posx
+	starty := posy
+	startd := d
+
+	count := 0
+	for {
+		count++
+		//hard cap timeout. if you hit this you're probably looping!
+		if count > 10000 {
+			return true
+		}
+		switch d {
+		case UP:
+			if posy == 0 {
+				return false
+			}
+			if split[posy-1][posx] == '#' || (posy-1 == locy && posx == locx) {
+				d = RIGHT
+			} else {
+				posy--
+			}
+		case RIGHT:
+			if posx == len(split[0])-1 {
+				return false
+			}
+			if split[posy][posx+1] == '#' || (posy == locy && posx+1 == locx) {
+				d = DOWN
+			} else {
+				posx++
+			}
+		case DOWN:
+			if posy == len(split)-1 {
+				return false
+			}
+			if split[posy+1][posx] == '#' || (posy+1 == locy && posx == locx) {
+				d = LEFT
+			} else {
+				posy++
+			}
+		case LEFT:
+			if posx == 0 {
+				return false
+			}
+			if split[posy][posx-1] == '#' || (posy == locy && posx-1 == locx) {
+				d = UP
+			} else {
+				posx--
+			}
+		default: //something fucked up
+		}
+		if posx == startx && posy == starty && d == startd {
+			return true
+		}
+	}
+}
+
 // this is so stupid i hate myself
 func part2BruteForce(inp string) int {
 	result := 0
-	split := make([][]rune, 1)
-	startx, starty := 0, 0
-	tempwhatevershutup := 0
-	for _, c := range inp {
-		switch c {
-		case '\n':
-			split = append(split, make([]rune, 0))
-			tempwhatevershutup++
-		default:
-			split[tempwhatevershutup] = append(split[tempwhatevershutup], c)
+	split := make([][]rune, 0, 100)
+	for i, line := range strings.Split(inp, "\n") {
+		if len(line) <= 0 {
+			break
+		}
+		split = append(split, make([]rune, 0, 150))
+		for _, c := range line {
+			split[i] = append(split[i], c)
 		}
 	}
-	split = split[:len(split)-1]
 
-	for i := range split {
-		for j := range split[i] {
-			if split[i][j] == '^' {
-				startx = j
-				starty = i
-				goto cont
+	startx, starty := func() (int, int) {
+		for i := range split {
+			for j := range split[i] {
+				if split[i][j] == '^' {
+					return j, i
+				}
 			}
 		}
-	}
-cont:
+		return 0, 0 //this should return an error but w/e
+	}()
+
 	for i := range split {
 		for j := range split[0] {
 			if split[i][j] != '.' {
@@ -364,9 +416,57 @@ cont:
 
 }
 
+// time to add concurrency? for reasons???
+func part2BruteForcePar(inp string) int {
+	result := 0
+	split := make([][]rune, 0, 100)
+	for i, line := range strings.Split(inp, "\n") {
+		if len(line) <= 0 {
+			break
+		}
+		split = append(split, make([]rune, 0, 150))
+		for _, c := range line {
+			split[i] = append(split[i], c)
+		}
+	}
+
+	startx, starty := func() (int, int) {
+		for i := range split {
+			for j := range split[i] {
+				if split[i][j] == '^' {
+					return j, i
+				}
+			}
+		}
+		return 0, 0 //this should return an error but w/e
+	}()
+	var wg sync.WaitGroup
+	var m sync.Mutex
+
+	for i := range split {
+		for j := range split[0] {
+			if split[i][j] != '.' {
+				continue
+			}
+			wg.Add(1)
+			go func() {
+				if checkForLoopArrPar(j, i, startx, starty, UP, split) {
+					m.Lock()
+					result++
+					m.Unlock()
+				}
+				wg.Done()
+			}()
+		}
+	}
+	wg.Wait()
+	return result
+}
+
 func main() {
 	inp, _ := os.ReadFile("input.txt")
 	fmt.Println(part1(string(inp)))
 	// fmt.Println(part2(string(inp)))
-	fmt.Println(part2BruteForce(string(inp)))
+	// fmt.Println(part2BruteForce(string(inp)))
+	fmt.Println(part2BruteForcePar(string(inp)))
 }
