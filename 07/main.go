eac68f5dd248cef79ebb0d2a9ab83ecd14e0fc1a
dave <davidmasq@gmail.com>
2024-12-06 22:44:13 -0800

    day 7 not bad

--- 07/main.go
+++ 07/main.go
@@ -3,14 +3,119 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func part1(inp string) int {
 	result := 0
+	for _, line := range strings.Split(inp, "\n") {
+		if len(line) <= 0 {
+			break
+		}
+		parsed := strings.Split(line, ": ")
+		target, _ := strconv.Atoi(parsed[0])
+		nums_s := strings.Split(parsed[1], " ")
+		//gather up the numbers
+		nums := make([]int, 0)
+		for _, num := range nums_s {
+			n, _ := strconv.Atoi(num)
+			nums = append(nums, n)
+		}
+		if isValid(target, 0, nums) {
+			result += target
+		}
+		// fmt.Println(result, ": ", line)
+	}
 	return result
 }
+
+func isValid(target int, acc int, inp []int) bool {
+	if len(inp) == 1 {
+		if acc+inp[0] == target || acc*inp[0] == target {
+			return true
+		}
+	}
+	if len(inp) > 1 {
+		if isValid(target, acc+inp[0], inp[1:]) {
+			return true
+		}
+		if acc == 0 {
+			return isValid(target, inp[0], inp[1:])
+		} else {
+			return isValid(target, acc*inp[0], inp[1:])
+
+		}
+	}
+	return false
+}
+
+func isValidP2(target int, acc int, inp []int) bool {
+	if len(inp) == 0 {
+		return acc == target
+	}
+	if len(inp) == 1 {
+		if acc+inp[0] == target || acc*inp[0] == target {
+			return true
+		}
+		acc_s := strconv.Itoa(acc)
+		inp_first_s := strconv.Itoa(inp[0])
+		merged, _ := strconv.Atoi(acc_s + inp_first_s)
+		return merged == target
+
+	}
+	if len(inp) > 1 {
+		if isValidP2(target, acc+inp[0], inp[1:]) {
+			return true
+		}
+		if acc == 0 {
+			if isValidP2(target, inp[0], inp[1:]) {
+				return true
+			}
+			inp_first_s := strconv.Itoa(inp[0])
+			inp_sec_s := strconv.Itoa(inp[1])
+			merged, _ := strconv.Atoi(inp_first_s + inp_sec_s)
+			if isValidP2(target, merged, inp[2:]) {
+				return true
+			}
+		} else {
+			if isValidP2(target, acc*inp[0], inp[1:]) {
+				return true
+			}
+			acc_s := strconv.Itoa(acc)
+			inp_first_s := strconv.Itoa(inp[0])
+			merged, _ := strconv.Atoi(acc_s + inp_first_s)
+			if isValidP2(target, merged, inp[1:]) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func part2(inp string) int {
 	result := 0
+	for _, line := range strings.Split(inp, "\n") {
+		if len(line) <= 0 {
+			break
+		}
+		parsed := strings.Split(line, ": ")
+		target, _ := strconv.Atoi(parsed[0])
+		nums_s := strings.Split(parsed[1], " ")
+		//gather up the numbers
+		nums := make([]int, 0)
+		for _, num := range nums_s {
+			n, _ := strconv.Atoi(num)
+			nums = append(nums, n)
+		}
+		if isValidP2(target, 0, nums) {
+			result += target
+			// fmt.Println(result, ": ", line, " success!")
+		} else {
+			// fmt.Println(result, ": ", line)
+
+		}
+	}
 	return result
 }
 
--- 07/main_test.go
+++ 07/main_test.go
@@ -4,19 +4,28 @@ import (
 	"testing"
 )
 
-var testinput string = ``
+var testinput string = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+`
 
 func TestP1(t *testing.T) {
 
 	result := part1(testinput)
-	if result != 0 {
-		t.Errorf("failed. got %d, should be 0", result)
+	if result != 3749 {
+		t.Errorf("failed. got %d, should be 3749", result)
 	}
 }
 func TestP2(t *testing.T) {
 
 	result := part2(testinput)
-	if result != 0 {
-		t.Errorf("failed. got %d, should be 0", result)
+	if result != 11387 {
+		t.Errorf("failed. got %d, should be 11387", result)
 	}
 }
